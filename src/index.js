c874763ed892c9976dbc10f09cb7716f50e76e72
pridekk <pridekk@gmail.com>
2020-03-25 22:36:44 +0900

    test2

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import Car from './Car'
 import { MDBContainer } from 'mdbreact'
 import {Card, CardActionArea, CardContent, CardMedia, Typography, Grid} from '@material-ui/core'
 import {withStyles} from '@material-ui/core/styles'
-const myFirstElement = <div><h1>Hello React!2</h1><br></br>
 
 
 const styles = theme => ({
+  back: {
+    backgroundColor: '#999'
+  },
   root: {
     maxWidth: 345, justify: 'center',
     alignItems: 'center',
@@ -36,9 +38,30 @@ class Garage extends Component {
     const carname = "Ford"
     const {classes} = this.props 
     return (
-      <Grid container spacing={3} direction="row" 
+      <Grid container spacing={3} direction="row" className={classes.back}
       >
         <Grid item xs  className={classes.paper}>
+        
+                <Typography gutterBottom variant="h5" component="h2">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                  across all continents except Antarctica
+                </Typography>
+              
+        </Grid>
+        <Grid item xs >
+          <Card className={classes.root}>
+              <CardMedia
+                className={classes.media}
+                image={process.env.PUBLIC_URL+"/logo.png"}
+                title="Contemplative Reptile"
+              />
+              
+            </Card>
+          </Grid>
+          <Grid item xs  className={classes.paper}>
         
                 <Typography gutterBottom variant="h5" component="h2">
                   Lizard
